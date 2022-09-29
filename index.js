0b41fdc21b725450cb81c32f59f483a7f0f7dbf6
Mohamed Kabbou <103792068+LifeTakerSimo@users.noreply.github.com>
2022-09-29 11:54:03 +0200

    Making it useful

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const inputel = document.getElementById("input-el")
-let myLeads = ["23,43"]
+let myLeads = []
 const inputbtn = document.getElementById("input-btn")
 const ulList =document.getElementById("ulList")
 
 
-input.addEventListener("click",function () {
+
+inputbtn.addEventListener("click",function () {
 
     myLeads.push(inputel.value)
+    renderLeads()
+    inputel.value = ""
+
 }) 
 
-for(let i=0;i>myLeads.length;i++ )
-{
-    ulList.innerHTML += "<li>" + myLeads[i] + "<li>"
+
+function renderLeads() {
+    let listItems = ""
+    for(let i=0 ; i < myLeads.length; i++ )
+    {
+     // Method 1   
+     // ulList.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+     // Method 2
+     //const li = document.createElement("li")
+     //li.textContent = myLeads[i]
+     //ulList.append(li)
+
+     //Method 3 
+     //   listItems += "<li> <a target='_blank' href = '" + myLeads[i] + "'> " + myLeads[i] + "</a> </li>"
+        listItems += ` 
+        <li> 
+            <a target='_blank' href = '${myLeads[i]}'> ${myLeads[i]}
+            </a> 
+        </li>`
+    }
+    ulList.innerHTML = listItems
 }
